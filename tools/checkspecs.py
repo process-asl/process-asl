3a91f4f992b216a398a9e1b896acd9646b82c43c
Salma <salmabougacha@hotmail.com>
2016-07-01 13:28:52 +0200

    update tests and specify running location of tools/checkspecs

--- tools/checkspecs.py
+++ tools/checkspecs.py
@@ -1,7 +1,8 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-"""Attempt to check each interface in nipype
+""" Attempt to check each nipype interface in procasl
+    Must be run in procasl directory
 """
 
 from __future__ import print_function
