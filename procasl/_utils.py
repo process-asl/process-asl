789a27e93f96ba7b968196b2b3780980d7c359cb
salma <salmabougacha@hotmail.com>
2017-01-14 17:58:48 +0100

    add dataset description function

--- procasl/_utils.py
+++ procasl/_utils.py
@@ -5,6 +5,22 @@ import numpy as np
 import nibabel
 
 
+def _get_dataset_descr(ds_name):
+    module_path = os.path.dirname(os.path.abspath(__file__))
+    fname = ds_name
+    try:
+        with open(os.path.join(module_path, 'description', fname + '.rst'))\
+                as rst_file:
+            descr = rst_file.read()
+    except IOError:
+        descr = ''
+
+    if descr == '':
+        print("Warning: Could not find dataset description.")
+
+    return descr
+
+
 def check_images(file1, file2):
     """Check that 2 images have the same affines and data shapes.
 
