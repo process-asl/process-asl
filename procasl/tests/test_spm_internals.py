05079b8f2997e0d69f3d311ba6085ebddd3cb68b
salma1601 <salmabougacha@hotmail.com>
2019-11-18 16:06:33 +0100

    added interface for spm_matrix

--- procasl/tests/test_spm_internals.py
+++ procasl/tests/test_spm_internals.py
@@ -1,5 +1,6 @@
 import os
 from nose import with_setup
+from nose.tools import assert_true
 from math import pi, cos, sin, sqrt
 
 import numpy as np
@@ -13,34 +14,80 @@ from nilearn.datasets.tests import test_utils as tst
 from procasl import spm_internals
 
 
-def spm_matrix(params):
-    """Run the spm function spm_matrix through Matlab.
+class SPMMatrixInputSpec(SPMCommandInputSpec):
+    params = traits.Either(
+        traits.List(traits.Float, minlen=6, maxlen=6),
+        traits.List(traits.Float, minlen=12, maxlen=12),
+        desc="Parameters of the transform, in the following order:"
+             "Tx, Ty, Tz, pitch, roll, yaw"
+             "and possibly 3 zooms and 3 shears")
+    mat_file = File(mandatory=True,
+                    desc="Matlab file to save the transform to")
 
-    Parameters
-    ==========
-    params : 1D numpy.ndarray, shape > 6
-        Parameters to convert to the affine transfomation matrix.
 
-    Returns
-    =======
-    affine : numpy.ndarray of shape (4, 4)
-        The affine transformation matrix.
+class SPMMatrixOutputSpec(TraitedSpec):
+    mat_file = File(exists=True, desc="Matlab file holding transform")
+
+
+class SPMMatrix(SPMCommand):
+    """ Uses SPM (spm_matrix) to convert 6 or 12 parameters to an affine
+    transfomation matrix and save it to a matlab file
+
+    Examples
+    --------
+    >>> import nipype.interfaces.spm.utils as spmu
+    >>> spm_matrix = spmu.SPMMatrix(matlab_cmd='matlab-spm8')
+    >>> spm_matrix.inputs.params = [12, 3., 4., 0.1, 0.02, -.1]
+    >>> spm_matrix.inputs.mat = 'rigid.mat'
+    >>> spm_matrix.run() # doctest: +SKIP
     """
-    # TODO write using _make_matlab_command to rely only on SPM
-    params_str = '[{0}, {1}, {2}, {3}, {4}, {5}]'.format(*params)
-    mat_path = '/tmp/matrix.mat'
 
-    # Generate the Matlab script
-    mlab = matlab.MatlabCommand()
-    mlab.inputs.script = "\naddpath '/i2bm/local/spm8-6313/' \n" + \
-                         "matrix = spm_matrix(" + params_str + ");\n" + \
-                         "save('" + mat_path + "', 'matrix')"
+    input_spec = SPMMatrixInputSpec
+    output_spec = SPMMatrixOutputSpec
 
-    # Run the script and load the output matrix
-    mlab.run()
-    mat_dict = loadmat(mat_path)
+    def _make_matlab_command(self, _):
+        """checks for SPM, generates script"""
+
+        script = """
+        matrix = spm_matrix([ %s ]);
+        save('%s' , 'matrix' );
+        """ % (
+            ', '.join(map(str, self.inputs.params)),
+            self.inputs.mat_file,
+        )
+        return script
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs["mat_file"] = os.path.abspath(self.inputs.mat_file)
+        return outputs
+
+
+@with_setup(tst.setup_tmpdata, tst.teardown_tmpdata)
+def test_spm_matrix():
+    mat_file = os.path.join(tst.tmpdir, 'anat_mapping.mat')
+    spm_matrix = SPMMatrix().run
+
+    # test with 6 parameters
+    out_spm_matrix = spm_matrix(params=[12., 3., 4., 0., 0., 0.],
+                                mat_file=mat_file)
+    assert_true(os.path.isfile(out_spm_matrix.outputs.mat_file))
+    mat_dict = loadmat(mat_file)
     affine = mat_dict['matrix']
-    return affine
+    expected_affine = np.eye(4)
+    expected_affine[:3, 3] = np.array([12., 3., 4.])
+    np.testing.assert_allclose(affine, expected_affine)
+
+    # test with 12 parameters
+    out_spm_matrix = spm_matrix(params=[0., 0., 0., 0., 0., 0.,
+                                        2., 1., 3., 0., 0., 0.],
+                                mat_file=mat_file)
+    assert_true(os.path.isfile(out_spm_matrix.outputs.mat_file))
+    mat_dict = loadmat(mat_file)
+    affine = mat_dict['matrix']
+    expected_affine = np.eye(4)
+    expected_affine[[0, 2], [0, 2]] = np.array([2., 3.])
+    np.testing.assert_allclose(affine, expected_affine)
 
 
 def spm_imatrix(affine):
@@ -78,7 +125,7 @@ def spm_imatrix(affine):
     return params
 
 
-class GetSpaceInputSpec(SPMCommandInputSpec):
+class SPMGetSpaceInputSpec(SPMCommandInputSpec):
     in_file = File(
         exists=True, mandatory=True,
         desc="target for reading the voxel-to-world"
@@ -87,25 +134,25 @@ class GetSpaceInputSpec(SPMCommandInputSpec):
                     desc="Matlab file to save the transform to")
 
 
-class GetSpaceOutputSpec(TraitedSpec):
+class SPMGetSpaceOutputSpec(TraitedSpec):
     mat_file = File(exists=True, desc="Matlab file holding transform")
 
 
-class GetSpace(SPMCommand):
+class SPMGetSpace(SPMCommand):
     """ Uses SPM (spm_get_space) to read the affine transform and save it
     to a matlab file
 
     Examples
     --------
     >>> import nipype.interfaces.spm.utils as spmu
-    >>> get_space = spmu.GetSpace(matlab_cmd='matlab-spm8')
-    >>> get_space.inputs.in_file = 'structural.nii'
-    >>> get_space.inputs.mat = 'func_to_struct.mat'
-    >>> get_space.run() # doctest: +SKIP
+    >>> spm_get_space = spmu.SPMGetSpace(matlab_cmd='matlab-spm8')
+    >>> spm_get_space.inputs.in_file = 'structural.nii'
+    >>> spm_get_space.inputs.mat = 'func_to_struct.mat'
+    >>> spm_get_space.run() # doctest: +SKIP
     """
 
-    input_spec = GetSpaceInputSpec
-    output_spec = GetSpaceOutputSpec
+    input_spec = SPMGetSpaceInputSpec
+    output_spec = SPMGetSpaceOutputSpec
 
     def _make_matlab_command(self, _):
         """checks for SPM, generates script"""
@@ -124,6 +171,21 @@ class GetSpace(SPMCommand):
         outputs["mat_file"] = os.path.abspath(self.inputs.mat_file)
         return outputs
 
+@with_setup(tst.setup_tmpdata, tst.teardown_tmpdata)
+def test_spm_get_space():
+    data = np.zeros((3, 6, 3))
+    affine = np.array([[-1.7, -0.9, -1.2, 16.2 ],
+                       [-0.9, -3.1, -2.3, 4.9],
+                       [-0.2, 1.8, 3.7, -2.1],
+                       [0., 0., 0., 1]])
+    img = nibabel.Nifti1Image(data, affine=affine)
+    in_file = os.path.join(tst.tmpdir, 'anat.nii')
+    mat_file = os.path.join(tst.tmpdir, 'anat_mapping.mat')
+    img.to_filename(in_file)
+    spm_get_space = SPMGetSpace().run
+    out_spm_get_space = spm_get_space(in_file=in_file, mat_file=mat_file)
+    assert_true(os.path.isfile(out_spm_get_space.outputs.mat_file))
+
 
 @with_setup(tst.setup_tmpdata, tst.teardown_tmpdata)
 def test_spm_affine():
@@ -136,9 +198,9 @@ def test_spm_affine():
     in_file = os.path.join(tst.tmpdir, 'anat.nii')
     mat_file = os.path.join(tst.tmpdir, 'anat_mapping.mat')
     img.to_filename(in_file)
-    get_space = GetSpace().run
-    out_get_space = get_space(in_file=in_file, mat_file=mat_file)
-    mat_dict = loadmat(out_get_space.outputs.mat_file)
+    spm_get_space = SPMGetSpace().run
+    out_spm_get_space = spm_get_space(in_file=in_file, mat_file=mat_file)
+    mat_dict = loadmat(out_spm_get_space.outputs.mat_file)
     affine_from_mat = mat_dict['matrix']
     np.testing.assert_allclose(spm_internals.spm_affine(in_file),
                                affine_from_mat)
